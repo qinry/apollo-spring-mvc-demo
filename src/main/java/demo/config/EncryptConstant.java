8424746c69bff3ba34ed9e9bafd42b6468c51467
qinry <lixiaoqin28@outlook.com>
2023-07-28 17:30:59 +0800

    fix

--- src/main/java/demo/config/EncryptConstant.java
+++
@@ -1,5 +0,0 @@
-package demo.config;
-
-public class EncryptConstant {
-    public static final String ENCRYPT_PROPERTY_SOURCE_NAME = "encryptPropertySource";
-}
--- src/main/java/demo/config/EncryptPropertySourceProcessor.java
+++ src/main/java/demo/config/EncryptPropertySourceProcessor.java
@@ -6,23 +6,18 @@ import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.core.PriorityOrdered;
-import org.springframework.core.env.CompositePropertySource;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.Environment;
-import org.springframework.core.env.MutablePropertySources;
-import org.springframework.core.env.PropertySource;
+import org.springframework.core.env.*;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.EncodedResource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePropertySource;
+import org.springframework.util.ObjectUtils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import static demo.config.EncryptConstant.ENCRYPT_PROPERTY_SOURCE_NAME;
 
 @Slf4j
 public class EncryptPropertySourceProcessor implements BeanFactoryPostProcessor, EnvironmentAware, PriorityOrdered {
@@ -31,21 +26,28 @@ public class EncryptPropertySourceProcessor implements BeanFactoryPostProcessor,
 
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-        if (this.environment.getPropertySources().contains(ENCRYPT_PROPERTY_SOURCE_NAME)) {
-            return;
-        }
-        // 将原有PropertySource用EncryptPropertySource包装后加进入新的CompositePropertySource
-        CompositePropertySource composite = new CompositePropertySource(ENCRYPT_PROPERTY_SOURCE_NAME);
-        MutablePropertySources propertySources = environment.getPropertySources();
-        Iterator<PropertySource<?>> iterator = propertySources.iterator();
-        while (iterator.hasNext()) {
-            PropertySource<?> propertySource = iterator.next();
-            composite.addPropertySource(new EncryptPropertySource(propertySource.getName(), propertySource));
-        }
-        // 默认将规定好路径的配置文件加载为EncryptPropertySource装进CompositePropertySource
-        // 如需使用规定路径外的配置文件，请使用@PropertySource进行额外加入属性即可
-        PathMatchingResourcePatternResolver pathMatchingResourcePatternResolver = new PathMatchingResourcePatternResolver();
         try {
+            List<PropertySource<?>> list = new ArrayList<>();
+            MutablePropertySources propertySources = this.environment.getPropertySources();
+            Iterator<PropertySource<?>> iterator = propertySources.iterator();
+            while (iterator.hasNext()) {
+                PropertySource<?> propertySource = iterator.next();
+                String name = propertySource.getName();
+
+                if (ObjectUtils.nullSafeEquals(name, "ApolloPropertySources")) {
+                    list.add(new EncryptPropertySource(propertySource.getName(), propertySource));
+                }
+                if (propertySource instanceof MapPropertySource) {
+                    list.add(new EncryptPropertySource(propertySource.getName(), propertySource));
+                }
+            }
+            // 将包装的PropertySource在Environment中进行替换
+            for (PropertySource<?> propertySource : list) {
+                propertySources.replace(propertySource.getName(), propertySource);
+            }
+            // 默认将规定好路径的配置文件加载为EncryptPropertySource装进CompositePropertySource
+            // 如需使用规定路径外的配置文件，请使用@PropertySource进行额外加入属性即可
+            PathMatchingResourcePatternResolver pathMatchingResourcePatternResolver = new PathMatchingResourcePatternResolver();
             List<Resource> resourceList = new ArrayList<>();
             List<String> locationPatterns = new ArrayList<>(Arrays.asList("classpath*:application*.properties"));
             for (String locationPattern : locationPatterns) {
@@ -59,10 +61,8 @@ public class EncryptPropertySourceProcessor implements BeanFactoryPostProcessor,
                 resourcePropertySourceList.add(new ResourcePropertySource(encodedResource));
             }
             for (ResourcePropertySource resourcePropertySource : resourcePropertySourceList) {
-                composite.addPropertySource(new EncryptPropertySource(resourcePropertySource.getName(), resourcePropertySource));
+                propertySources.addLast(new EncryptPropertySource(resourcePropertySource.getName(), resourcePropertySource));
             }
-            // CompositePropertySource加进Environment
-            environment.getPropertySources().addFirst(composite);
         } catch (Exception e) {
             log.error("Exception!", e);
         }
